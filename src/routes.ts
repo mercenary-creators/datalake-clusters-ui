ab26d82442f04405f999d0f87d4f424b335a8c2d
Dean Jones <deansjones@gmail.com>
2019-06-08 01:02:08 -0400

    0.0.3-SNAPSHOT

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,21 @@
+/*
+ * Copyright (c) 2019, Mercenary Creators Company. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {RouteProps} from 'react-router';
-import {ClustersDemo, DatalakeDemo} from './pages/demos';
+import {ClustersDemo, DatalakeDemo, ServicesDemo} from './pages/demos';
 
 interface IDemoRoute extends RouteProps {
     path: string;
@@ -16,5 +32,10 @@ export const routes: IDemoRoute[] = [
         label: 'Datalake',
         path: '/datalake',
         component: DatalakeDemo
+    },
+    {
+        label: 'Services',
+        path: '/services',
+        component: ServicesDemo
     },
 ];
